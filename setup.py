08b73d665411b2cdf384403a2e1755bc54edfbb5
sophia <41456914+i0bs@users.noreply.github.com>
2022-08-08 09:12:47 -0400

    chore: version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fp):
     return open(path.join(path.dirname(__file__), fp)).read()
 
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 README = read("README.md")
 # REQUIREMENTS = open("requirements.txt", "r").read().strip().splitlines()
 AUTHOR_NAME = "i0"
