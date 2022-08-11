4758c11ff6a69e597eda3a301747be8792adc5bc
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-08-11 16:37:33 +0200

    feat: implement `StageInstance` object (#38)
    * feat: implement stage instance
    
    * Update retux/client/resources/stage_instance.py
    
    Co-authored-by: sophia <41456914+i0bs@users.noreply.github.com>
    
    * Update retux/client/resources/stage_instance.py
    
    Co-authored-by: sophia <41456914+i0bs@users.noreply.github.com>
    
    * Update retux/client/resources/stage_instance.py
    
    Co-authored-by: sophia <41456914+i0bs@users.noreply.github.com>
    
    * Update retux/client/resources/stage_instance.py
    
    Co-authored-by: sophia <41456914+i0bs@users.noreply.github.com>
    
    * Update retux/client/resources/stage_instance.py
    
    Co-authored-by: sophia <41456914+i0bs@users.noreply.github.com>
    
    * Update retux/client/resources/stage_instance.py
    
    Co-authored-by: sophia <41456914+i0bs@users.noreply.github.com>
    
    * Update retux/client/resources/stage_instance.py
    
    Co-authored-by: sophia <41456914+i0bs@users.noreply.github.com>
    
    * Update retux/client/resources/stage_instance.py
    
    Co-authored-by: sophia <41456914+i0bs@users.noreply.github.com>
    
    * Update stage_instance.py
    
    Co-authored-by: sophia <41456914+i0bs@users.noreply.github.com>

---
+++ retux/client/resources/stage_instance.py
@@ -0,0 +1,69 @@
+from .abc import Snowflake
+from ...utils.converters import optional_c
+
+from attrs import define, field
+from enum import IntEnum
+
+__all__ = (
+    "StageInstance",
+    "StagePrivacyLevel",
+)
+
+
+class StagePrivacyLevel(IntEnum):
+    """
+    An enumeration representing the privacy levels of a stage in Discord.
+
+    Constants
+    ---------
+    PUBLIC
+        The Stage instance is visible publicly. (deprecated)
+    GUILD_ONLY
+        The Stage instance is visible to only guild members.
+    """
+
+    PUBLIC = 1
+    """The stage instance is visible publicly. (deprecated)"""
+    GUILD_ONLY = 2
+    """The stage instance is visible to only guild members."""
+
+
+@define
+class StageInstance:
+    """
+    A class object representing a stage instance from Discord.
+
+    Attributes
+    ----------
+    id : `Snowflake`
+        The ID of this stage instance.
+    guild_id : `Snowflake`
+        The guild ID of the associated stage channel.
+    channel_id : `Snowflake`
+        The ID of the associated stage channel.
+    topic : `str`
+        The topic of the stage instance (1-120 characters).
+    privacy_level : `StagePrivacyLevel`
+        The privacy level of the Stage instance.
+    discoverable_disabled : `bool`
+        Whether or not Stage Discovery is disabled (deprecated).
+    guild_scheduled_event_id : `Snowflake`, optional
+        The ID of the scheduled event for this Stage instance.
+    """
+
+    id: str | Snowflake = field(converter=Snowflake)
+    """The ID of this stage instance."""
+    guild_id: str | Snowflake = field(converter=Snowflake)
+    """The guild ID of the associated stage channel."""
+    channel_id: str | Snowflake = field(converter=Snowflake)
+    """The ID of the associated stage channel."""
+    topic: str = field()
+    """The topic of the stage instance (1-120 characters)."""
+    privacy_level: int | StagePrivacyLevel = field(converter=StagePrivacyLevel)
+    """The privacy level of the stage instance."""
+    discoverable_disabled: bool = field(default=False)
+    """Whether or not Stage Discovery is disabled (deprecated)."""
+    guild_scheduled_event_id: str | Snowflake | None = field(converter=optional_c(Snowflake))
+    """The ID of the scheduled event for this stage instance."""
+
+Stage = StageInstance
